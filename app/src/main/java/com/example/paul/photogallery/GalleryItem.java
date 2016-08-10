f3e8c184220d4ef56a077c1f96df8e3a94736e8e
geniusofsimplicity <genius.of.simplicity@gmail.com>
2016-08-10 10:03:12 +0200

    Initial upload

---
+++ app/src/main/java/com/example/paul/photogallery/GalleryItem.java
@@ -0,0 +1,59 @@
+package com.example.paul.photogallery;
+
+import android.net.Uri;
+
+/**
+ * Created by Paul on 30.06.2016.
+ */
+public class GalleryItem {
+    private String mCaption;
+    private String mId;
+    private String mUrl;
+
+    public String getOwner() {
+        return mOwner;
+    }
+
+    public void setOwner(String owner) {
+        mOwner = owner;
+    }
+
+    private String mOwner;
+
+    public Uri getPhotoPageUri(){
+        return Uri.parse("http://www.flickr.com/photos/")
+                .buildUpon()
+                .appendPath(mOwner)
+                .appendPath(mId)
+                .build();
+    }
+
+    @Override
+    public String toString(){
+        return mCaption;
+    }
+
+    public String getCaption() {
+        return mCaption;
+    }
+
+    public void setCaption(String caption) {
+        mCaption = caption;
+    }
+
+    public String getUrl() {
+        return mUrl;
+    }
+
+    public void setUrl(String url) {
+        mUrl = url;
+    }
+
+    public String getId() {
+        return mId;
+    }
+
+    public void setId(String id) {
+        mId = id;
+    }
+}
---
+++ app/src/main/java/com/example/paul/photogallery/PollService.java
@@ -0,0 +1,135 @@
+package com.example.paul.photogallery;
+
+import android.app.Activity;
+import android.app.AlarmManager;
+import android.app.IntentService;
+import android.app.Notification;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.content.res.Resources;
+import android.net.ConnectivityManager;
+import android.os.SystemClock;
+import android.support.v4.app.NotificationCompat;
+import android.support.v4.app.NotificationManagerCompat;
+import android.util.Log;
+
+import java.util.List;
+
+/**
+ * Created by Paul on 04.07.2016.
+ */
+public class PollService extends IntentService {
+    private static final String TAG = "PollService";
+
+    private static final int POLL_INTERVAL = 1000 * 60; // 60 seconds
+
+    public static final String ACTION_SHOW_NOTIFICATION = "com.example.paul.photogallery.SHOW_NOTIFICATION";
+    public static final String PERM_PRIVATE = "com.example.paul.photogallery.PRIVATE";
+    public static final String REQUEST_CODE = "REQUEST_CODE";
+    public static final String NOTIFICATION = "NOTIFICATION";
+
+    public static Intent newIntent(Context context){
+        return new Intent(context, PollService.class);
+    }
+
+    public static void setServiceAlarm(Context context, boolean isOn){
+        Intent i = PollService.newIntent(context);
+        PendingIntent pi = PendingIntent.getService(context, 0, i, 0);
+
+        AlarmManager alarmManager = (AlarmManager)
+                context.getSystemService(Context.ALARM_SERVICE);
+
+        if(isOn){
+            alarmManager.setInexactRepeating(AlarmManager.ELAPSED_REALTIME, SystemClock.elapsedRealtime(), POLL_INTERVAL, pi);
+        }else {
+            alarmManager.cancel(pi);
+            pi.cancel();
+        }
+
+        QueryPreferences.setAlarmOn(context, isOn);
+    }
+
+    public static boolean isServiceAlarmOn(Context context){
+        Intent i = PollService.newIntent(context);
+        PendingIntent pi = PendingIntent.getService(context, 0, i, PendingIntent.FLAG_NO_CREATE);
+        return pi != null;
+    }
+
+    public PollService(){
+        super(TAG);
+    }
+
+    @Override
+    protected void onHandleIntent(Intent intent) {
+        if(!isNetworkAvailableAndConnected()){
+            return;
+        }
+
+        String query = QueryPreferences.getStoredQuery(this);
+        String lastResultId = QueryPreferences.getLastResultId(this);
+        List<GalleryItem> items;
+
+        if(query == null){
+            items = new FlickrFetchr().fetchRecentPhotos();
+        }else{
+            items = new FlickrFetchr().searchPhotos(query);
+        }
+
+        if(items.size() == 0){
+            return;
+        }
+
+        String resultId = items.get(0).getId();
+        if(resultId.equals(lastResultId)){
+            Log.i(TAG, "Got an old result: " + resultId);
+        }else{
+            Log.i(TAG, "Got a new result: " + resultId);
+
+            Resources resources = getResources();
+            Intent i = PhotoGalleryActivity.newIntent(this);
+            PendingIntent pi = PendingIntent.getActivity(this, 0, i, 0);
+
+            Notification notification = new NotificationCompat.Builder(this)
+                    .setTicker(resources.getString(R.string.new_pictures_title))
+                    .setSmallIcon(android.R.drawable.ic_menu_report_image)
+                    .setContentTitle(resources.getString(R.string.new_pictures_title))
+                    .setContentText(resources.getString(R.string.new_pictures_text))
+                    .setContentIntent(pi)
+                    .setAutoCancel(true)
+                    .build();
+
+            showBackgroundNotification(0, notification);
+        }
+
+        QueryPreferences.setLastResultId(this, resultId);
+    }
+
+    private void showBackgroundNotification(int requestCode, Notification notification){
+        Intent i = new Intent(ACTION_SHOW_NOTIFICATION);
+        i.putExtra(REQUEST_CODE, requestCode);
+        i.putExtra(NOTIFICATION, notification);
+        sendOrderedBroadcast(i, PERM_PRIVATE, null, null, Activity.RESULT_OK, null, null);
+    }
+
+    private boolean isNetworkAvailableAndConnected(){
+
+//        int permissionCheck = ContextCompat.checkSelfPermission(this,
+//                Manifest.permission.ACCESS_NETWORK_STATE);
+//        if (permissionCheck != PackageManager.PERMISSION_GRANTED) {
+//            ActivityCompat.requestPermissions(this,
+//                    new String[]{Manifest.permission.ACCESS_NETWORK_STATE},
+//                    REQUEST_CODE_ACCESS_NETWORK_STATE); // define this constant yourself
+//        } else {
+//            // you have the permission
+//        }
+
+        ConnectivityManager cm = (ConnectivityManager) getSystemService(CONNECTIVITY_SERVICE);
+
+
+
+        boolean isNetworkAvailable = cm.getActiveNetworkInfo() != null;
+        boolean isNetworkConnected = isNetworkAvailable && cm.getActiveNetworkInfo().isConnected();
+        return isNetworkConnected;
+    }
+}
